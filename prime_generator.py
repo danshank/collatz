c5fa5228db361caf34a7a5a4d49ff1ed3ca0116e
Daniel Shank <Daniel Shank>
2016-06-16 20:22:43 -0400

    Add a function which takes in a number n and outputs a list of the first n primes.

---
+++ prime_generator.py
@@ -0,0 +1,22 @@
+def make_n_primes(n):
+	primes = []
+	num = 2
+	prime_stat = "maybe"
+	while len(primes) < n:
+		print "Now evaluating %d" % num
+		for i in primes:
+			if num % i == 0:
+				print "%d is a multple of %d and is not a prime" % (num, i)
+				num = num + 1
+				prime_stat = "no"
+				break
+			else:
+				None
+		if prime_stat == "no":
+			prime_stat = "maybe"
+		else:
+			print "Adding %d to the list of primes" % num
+			primes.append(num)
+			num = num + 1
+			prime_stat = "maybe"
+	return primes
