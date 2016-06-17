487e7e45e4318a989e25fb99b6a0877443cdcd97
Daniel Shank <Daniel Shank>
2016-06-17 14:21:19 -0400

    Remove trouble-shooting text messages from printing

--- prime_generator.py
+++ prime_generator.py
@@ -3,10 +3,10 @@ def make_n_primes(n):
 	num = 2
 	prime_stat = "maybe"
 	while len(primes) < n:
-		print "Now evaluating %d" % num
+		#print "Now evaluating %d" % num
 		for i in primes:
 			if num % i == 0:
-				print "%d has a prime factor of %d and is not a prime" % (num, i)
+				#print "%d has a prime factor of %d and is not a prime" % (num, i)
 				num = num + 1
 				prime_stat = "no"
 				break
@@ -15,7 +15,7 @@ def make_n_primes(n):
 		if prime_stat == "no":
 			prime_stat = "maybe"
 		else:
-			print "Adding %d to the list of primes" % num
+			#print "Adding %d to the list of primes" % num
 			primes.append(num)
 			num = num + 1
 			prime_stat = "maybe"
